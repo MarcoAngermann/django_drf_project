07a5735891ce8b8b2c1bd3f0b76fbcd720141f13
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-11-07 17:36:15 +0100

    serializers & POST

--- market_app/api/serializers.py
+++ market_app/api/serializers.py
@@ -1,8 +1,15 @@
 from rest_framework import serializers
+from market_app.models import Market
 
-class CommentSerializer(serializers.Serializer):
+class MarketSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
     location = serializers.CharField(max_length=255)
     description = serializers.CharField()
     net_worth = serializers.DecimalField(max_digits=100, decimal_places=2)
+
+    def create(self, validated_data):
+        return Market.objects.create(**validated_data)
+
+    def update(self, validated_data):
+        return Market.objects.create(**validated_data)
