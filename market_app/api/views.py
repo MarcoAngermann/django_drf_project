50026d00050f7e1038aa9cf28bbd1386a4c02448
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-11-07 18:32:32 +0100

    serializers & PUT

--- market_app/api/views.py
+++ market_app/api/views.py
@@ -17,3 +17,30 @@ def markets_view(request):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def market_single_view(request, pk):
+    if request.method == 'GET':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+       
+
+
+    if request.method == 'DELETE':
+        market = Market.objects.get(pk=pk)
+        serializer = MarketSerializer(market)
+        market.delete()
+        return Response(serializer.data)
