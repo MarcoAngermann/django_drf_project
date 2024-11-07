4f46ebe420ddbb6d060de75745cc587e03dfc536
MarcoAngermann <MarcoAngermann@hotmail.de>
2024-11-07 16:28:37 +0100

    POST with try and exept

--- market_app/api/views.py
+++ market_app/api/views.py
@@ -1,6 +1,13 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view()
+@api_view(['GET', 'POST'])
 def first_view(request):
-    return Response({"message": "Hello, world!"})
+    if request.method == 'GET':
+        return Response({"message": "Hello, world!"})
+    if request.method == 'POST':
+        try:
+            msg = request.data["message"]
+            return Response({"your_message": msg})
+        except:
+            return Response({"your_message": "error"})
